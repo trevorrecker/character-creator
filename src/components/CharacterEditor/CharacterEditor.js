925b54b7a4c14fc7f613757b555e359ca59b2da6
Trevor Recker <trevorrecker@gmail.com>
2021-11-14 20:40:06 -0700

    Exercise 4

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -31,6 +31,8 @@ function App() {
   return (
     <main className={styles.characterEditor}>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
+        <div className={styles.perspectiveBackground} />
+        
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
